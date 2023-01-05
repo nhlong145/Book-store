e3d99e733a1f9a0c31223822cf655be2efbf5ef6
longnguyentel <long.nguyen@tel.com>
2023-01-05 12:03:15 +0700

    complete code

--- books/views.py
+++ books/views.py
@@ -8,7 +8,7 @@ PermissionRequiredMixin # new
 )
 from django.views.generic import ListView, DetailView # new
 from .models import Book
-
+from django.db.models import Q # new
 
 class BookListView(LoginRequiredMixin, ListView):
     model = Book
@@ -23,3 +23,17 @@ class BookDetailView(LoginRequiredMixin,PermissionRequiredMixin, DetailView): # 
     template_name = 'books/book_detail.html'
     login_url = 'account_login' # new
     permission_required = 'books.special_status' # new
+
+
+class SearchResultsListView(ListView): # new
+    model = Book
+    context_object_name = 'book_list'
+    template_name = 'books/search_results.html'
+    # queryset = Book.objects.filter(title__icontains='beginners') # new
+
+    def get_queryset(self): # new
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+                Q(title__icontains=query) | Q(author__icontains=query)
+            )
+
